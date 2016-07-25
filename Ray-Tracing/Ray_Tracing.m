0f419b50fe78ef3a54519bb01998ba60ec85c014
David Cyncynates <dcc57@case.edu>
2016-07-25 16:52:51 -0400

    Code is good. Needs color

--- Ray-Tracing/Ray_Tracing.m
+++ Ray-Tracing/Ray_Tracing.m
@@ -1,91 +1,131 @@
 clc
 clear
 format long
-PointMatrix=[];
-PointMatrix=load('20_PointsOnASphere.txt');
+%EvenlyDistributedPoints=[];
+%EvenlyDistributedPoints=load('130_Points_On_A_Sphere.txt');
 counter=1;
 macrocounter=1;
-LengthofPointmatrix=length(PointMatrix);
-RayDirectionVectors=[];
+%LengthofPointmatrix=length(EvenlyDistributedPoints);
+%RayDirectionVectors=zeros(LengthofPointmatrix/3,3);
 SecondPhase=[];
-AlphaList=[];
-BetaList=[];
-Flip=[]; %1 means AlphaRay->2 Pi-AlphaRay 0 means no flip
-TheHolyHandArray=[]; %Angles of Incidence
-Bees=[]; %Do again?
-ThePlebeianThinksTheyreDone=[]; %Angle (as measured from the origin) at which the ray hits the surface of the Earth
-for i=1:LengthofPointmatrix %This loop unpacks the direction vectors from a text file 'M_PointsOnASphere.txt'
-    RayDirectionVectors(macrocounter,counter)=PointMatrix(i);
-    if counter==3
-        counter=1;
-        macrocounter=macrocounter+1;
-    else
-        counter=counter+1;
+%for i = 1 : LengthofPointmatrix %This loop unpacks the direction vectors from a text file 'M_PointsOnASphere.txt'
+%    RayDirectionVectors(macrocounter,counter) = EvenlyDistributedPoints(i);
+%    if counter == 3
+%        counter = 1;
+%        macrocounter = macrocounter + 1;
+%    else
+%        counter = counter + 1;
+%    end
+%end
+RE = 6; %input('What is the radius of the earth? (meters) ');
+D = 2; %input('What is the distance of closest approach of the macro to the center? (meters) ');
+N = 400; %input('How many points along this line should we consider? ') + 1;
+M = 100; %input('How many rays will each point emit? ');
+Rmantle = 2.5; %input('What is the radius of the mantle? (meters) ');
+ac = 1; %sqrt(input('Inside the core, assuming that the velocity field has the form a^2-b^2*r^2, what is a^2? '));
+bc = .1; %sqrt(input('Inside the core, assuming that the velcoity field has the form a^2-b^2*r^2, what is b^2? '));
+am = 1; %sqrt(input('Outside the core, assuming that the velocity field has the form a^2-b^2*r^2, what is a^2? '));
+bm = .01; %sqrt(input('Outside the core, assuming that the velocity field has the form a^2-b^2*r^2, what is b^2? '));
+RayDirectionVectors = zeros(M,N,3);
+for n = 1 : N
+    for m = 1 : M
+        Th = 2*pi*rand;
+        Ph = asin(-1+2*rand);
+        [x,y,z] = sph2cart(Th,Ph,1);
+        RayDirectionVectors(m,n,:) = [x,y,z];
     end
 end
-RE=input('What is the radius of the earth? (meters) ');
-D=input('What is the distance of closest approach of the macro to the center? (meters) ');
-N=input('How many points along this line should we consider? ');
-Rmantle=input('What is the radius of the mantle? (meters) ');
-ac=sqrt(input('Inside the core, assuming that the velocity field has the form a^2-b^2*r^2, what is a^2? '));
-bc=sqrt(input('Inside the core, assuming that the velcoity field has the form a^2-b^2*r^2, what is b^2? '));
-am=sqrt(input('Outside the core, assuming that the velocity field has the form a^2-b^2*r^2, what is a^2? '));
-bm=sqrt(input('Outside the core, assuming that the velocity field has the form a^2-b^2*r^2, what is b^2? '));
-vm=input('What is the velocity of p-waves just inside the mantle? ');
-vc=input('What is the velocity of p-waves just outside the mantle? ');
-M=LengthofPointmatrix/3;
-%VARIABLE DECLARATION SECTION
-%IT'S THE FINAAAAAAAL COUNTDOWN
-L=2*((((RE)^2)-((D)^2))^0.5); %The length of the path through the earth.
+vm = (am^2)-(bm^2)*(Rmantle)^2;
+vc = (ac^2)-(bc^2)*(Rmantle)^2;
+CriticalAngle = asin(vc/vm);
+%M = LengthofPointmatrix/3; %The number of rays propagating from each point
+AnglesOfIncidence = zeros(M,N); %Angles of Incidence
+AngleAtCoreMantleBoundary = zeros(M,N); %Angular displacement for rays incident on the core-mantle boundary
+AngleAtSurface=zeros(M,N); %Angular displacement for rays incident on the surface of the earth
+HitCore = zeros(M,N); %Do again?
+HitSurface = zeros(M,N); %Did the ray get totally internally reflected?
+AlphaList = zeros(1,N); %A running list of Alpha's. These are the angles by which the point sources are rotated.
+BetaList = zeros(M,N); %A running list of Beta's.
+ImpactPositionList = zeros(M*N,3); %The final xyz coordinates of the rays. This is an MxN array with a triplet for each entry.
+L = 2*((((RE)^2)-((D)^2))^0.5); %The length of the path through the earth.
 for n=1:N-1
     x0 = X0(L,N,n,D); %X0 is the function which tells you how far from the origin the point source is, while x0 is the value for a given n
     l = ((L/N)*(n-(N/2))); %l is the distance along the trajectory from the y-axis to the point source before rotation
     Alpha = atan2(D,l); %Alpha is the angle we rotate by so that the point souce is on the x-axis
-    AlphaList = cat(1,AlphaList,Alpha); %AlphaList keeps track of the Alpha's in a column vector whose length is N
+    AlphaList(n) = Alpha; %AlphaList keeps track of the Alpha's in a row vector whose length is N
     RotAlpha = [cos(Alpha),sin(Alpha),0;-sin(Alpha),cos(Alpha),0;0,0,1]; %This is the rotation matrix corresponding to a rotation about the z-axis by Alpha radians CLOCKWISE
-    disp('AHHHHHHHHHHH')
         for m = 1 : M
-        AlphaRayDirectionVectors=RotAlpha*[(RayDirectionVectors(m,1)+l);(RayDirectionVectors(m,2)+D);RayDirectionVectors(m,3)]; %We now rotate the ray direction vector by Alpha so that its tail lies on the x axis
-        Beta=atan2(AlphaRayDirectionVectors(3),AlphaRayDirectionVectors(2)); %Beta is the angle through which we must rotate so that the direction vector lies in the xy-plane
-        BetaList=cat(1,BetaList,Beta); %BetaList keeps track of all the Beta's in a column vector whose length is M
-        RotBeta=[1,0,0;0,cos(Beta),sin(Beta);0,-sin(Beta),cos(Beta)]; %This is the rotation matrix corresponding to a rotation by Beta about the x-axis CLOCKWISE
-        BetaAlphaRayDirectionVectors=RotBeta*[AlphaRayDirectionVectors(1);AlphaRayDirectionVectors(2);AlphaRayDirectionVectors(3)]; %These are the coordinates of the initial ray direction vectors after rotation into the xy-plane
-        AlphaRay=atan2(BetaAlphaRayDirectionVectors(2),(BetaAlphaRayDirectionVectors(1)-x0)); %This is the angle that the ray direction vectors make with the x-axis
-        disp('AHHHHHHHHHHH')
-        if AlphaRay > pi
-            AlphaRay = 2*pi - AlphaRay;
-            Flip = cat(1,Flip,1);
-            disp('dips')
-        else
-            Flip = cat(1,Flip,0);
-            disp('dops')
-        end
-        if and(x0>Rmantle , AlphaRay > pi/2) %The ray has the potential to hit the mantle
-            Smantle=SR(Rmantle,am,bm,x0,AlphaRay,1,1,0);  
-            if abs(Smantle) > real(Smantle) %The ray misses the mantle
+        AlphaRayDirectionVectors(m,n,:) = RotAlpha*[(RayDirectionVectors(m,n,1)+l);(RayDirectionVectors(m,n,2)+D);RayDirectionVectors(m,n,3)]; %We now rotate the ray direction vector by Alpha so that its tail lies on the x axis
+        Beta = atan2(AlphaRayDirectionVectors(m,n,3),AlphaRayDirectionVectors(m,n,2)); %Beta is the angle through which we must rotate so that the direction vector lies in the xy-plane
+        BetaList(m,n) = Beta; %BetaList keeps track of all the Beta's in a column vector whose length is M
+        RotBeta = [1,0,0;0,cos(Beta),sin(Beta);0,-sin(Beta),cos(Beta)]; %This is the rotation matrix corresponding to a rotation by Beta about the x-axis CLOCKWISE
+        BetaAlphaRayDirectionVectors(m,n,:) = RotBeta*[AlphaRayDirectionVectors(m,n,1);AlphaRayDirectionVectors(m,n,2);AlphaRayDirectionVectors(m,n,3)]; %These are the coordinates of the initial ray direction vectors after rotation into the xy-plane
+        AlphaRay = atan2(BetaAlphaRayDirectionVectors(m,n,2),(BetaAlphaRayDirectionVectors(m,n,1)-x0)); %This is the angle that the ray direction vectors make with the x-axis
+        if and(x0 > Rmantle , AlphaRay > pi/2) %The ray has the potential to hit the mantle
+            AlphaRay = pi - AlphaRay;
+            Smantle = SR(Rmantle,am,bm,x0,AlphaRay,0,0,1);
+            if abs(imag(Smantle))>0 %The ray misses the mantle
                 Searth=SR(RE,am,bm,x0,AlphaRay,0,0,0);
-                ThePlebeianThinksTheyreDone(m,n) = theta(Searth,am,bm,AlphaRay,x0,0,0,0);
-                Bees(m,n) = 0;
+                AngleAtSurface(m,n) = theta(Searth,am,bm,AlphaRay,x0,0,0,0);
+                HitSurface(m,n) = 1;
             else %The ray reflects off the mantle
-                TheHolyHandArray(m,n) = 2*pi - asin(GOD(Smantle,am,bm,AlphaRay,x0,1,1,0));
-                Bees(m,n) = 1;
+                AnglesOfIncidence(m,n) = 2*pi - asin(GOD(Smantle,am,bm,AlphaRay,x0,0,0,1));
+                AngleAtCoreMantleBoundary(m,n) = theta(Smantle,am,bm,AlphaRay,x0,0,0,1);
+                HitCore(m,n) = 1;
             end
         elseif and(x0>Rmantle , AlphaRay <= pi/2) %The ray does not have the potential to hit the mantle
-            %(1,0,0)
-            Searth=SR(RE,am,bm,x0,AlphaRay,0,0,0);
-            ThePlebeianThinksTheyreDone(m,n) = theta(Searth,am,bm,AlphaRay,x0,0,0,0);
-            Bees(m,n) = 0;
+            Searth=SR(RE,am,bm,x0,AlphaRay,1,0,0);
+            AngleAtSurface(m,n) = theta(Searth,am,bm,AlphaRay,x0,1,0,0);
+            HitSurface(m,n) = 1;
         elseif and(x0<=Rmantle , AlphaRay > pi/2)
-            %(0,0,0)
-            Bees(m,n) = 1;
+            AlphaRay = pi - AlphaRay;
+            Smantle = SR(Rmantle,ac,bc,x0,AlphaRay,0,0,0);
+            god = GOD(Smantle,ac,bc,AlphaRay,x0,0,0,0);
+            angleofincidence = asin(god);
+            if CriticalAngle > angleofincidence
+                AnglesOfIncidence(m,n) = asin((vm/vc)*god);
+                AngleAtCoreMantleBoundary(m,n) = theta(Smantle,ac,bc,AlphaRay,x0,0,0,0);
+                HitCore(m,n) = 1;
+            end
         elseif and(x0<=Rmantle , AlphaRay <= pi/2) 
-            %(1,0,0)
-            Bees(m,n) = 1;
+            Smantle = SR(Rmantle,ac,bc,x0,AlphaRay,1,0,0);
+            god = GOD(Smantle,ac,bc,AlphaRay,x0,1,0,0);
+            angleofincidence = asin(god);
+            if CriticalAngle > angleofincidence
+                AnglesOfIncidence(m,n) = asin((vm/vc)*god);
+                AngleAtCoreMantleBoundary(m,n) = theta(Smantle,ac,bc,AlphaRay,x0,1,0,0);
+                HitCore(m,n) = 1;
+            end
         end
         end
 end
-%{
-    Note that using theta_mantle values, those are an additional rotation
-    to be compensated for.  Such angle transformations are outside the
-    scope of this program.
-%}
+for n = 1 : N - 1
+    for m = 1 : M
+        if HitCore(m,n) == 1 %note that the angle of departure is always between 0 and pi/2
+            Searth = SR(RE,am,bm,Rmantle,AnglesOfIncidence(m,n),1,0,0);
+            AngleAtSurface(m,n) = theta(Searth,am,bm,AnglesOfIncidence(m,n),Rmantle,1,0,0)+AngleAtCoreMantleBoundary(m,n);
+            HitSurface(m,n) = 1;
+        end
+    end
+end
+j = 0;
+for n = 1 : N - 1
+    for m = 1 : M
+        if HitSurface(m,n) == 1
+            j = j + 1;
+            ImpactPositionBeforeRotation = [cos(AngleAtSurface(m,n));sin(AngleAtSurface(m,n));0];
+            RotBackBeta = [1,0,0;0,cos(BetaList(m,n)),-sin(BetaList(m,n));0,sin(BetaList(m,n)),cos(BetaList(m,n))];
+            ImpactPositionAfterFirstRotation = RotBackBeta * ImpactPositionBeforeRotation;
+            RotBackAlpha = [cos(AlphaList(n)),-sin(AlphaList(n)),0;sin(AlphaList(n)),cos(AlphaList(n)),0;0,0,1];
+            ImpactPosition = RotBackAlpha * ImpactPositionAfterFirstRotation;
+            for i = 1 : 3
+                ImpactPositionList(j,i) = ImpactPosition(i);
+            end
+        end
+    end
+end
+ImpactPositionList = ImpactPositionList(1:j , : );
+%Now we need to convert this into a pretty picture.
+figure
+scatter3(ImpactPositionList(:,1),ImpactPositionList(:,2),ImpactPositionList(:,3),'.')
+
--- Ray-Tracing/r.m
+++ Ray-Tracing/r.m
@@ -1,4 +1,8 @@
 function output = r(s,a,b,AlphaRay,x0,s1,~,~)
 %This is the radial coordinate of the ray as a function of the parameter s
- output = (1/(b*sqrt((a^2)*((exp(2*a*b*s) + 1)^2) + (b^2)*((exp(2*a*b*s) - 1)^2)*(x0^2) + 2*((-1)^(s1))*a*b*(exp(4*a*b*s) - 1)*x0*abs(cos(AlphaRay)))))*a*sqrt((a^2)*((exp(2*a*b*s) - 1)^2) + (b^2)*((exp(2*a*b*s) + 1)^2)*(x0^2) + 2*((-1)^(s1))*a*b*(exp(4*a*b*s) - 1)*x0*abs(cos(AlphaRay)));
+output = ((1+exp(1).^(2.*s)).^2+((-1)+exp(1).^(2.*s)).^2.*x0.^2+(-2).*((-1) ...
+  +exp(1).^(4.*s)).*x0.*abs(cos(\[Alpha]))).^(-1/2).*(((-1)+exp(1).^(\
+2.*s)) ...
+.^2+(1+exp(1).^(2.*s)).^2.*x0.^2+(-2).*((-1)+exp(1).^(4.*s)).*x0.* ...
+  abs(cos(\[Alpha]))).^(1/2);
 end
