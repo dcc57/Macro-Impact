f1f549fb69d6f68a52b314b1ff66ef80a0f9e737
David Cyncynates <dcc57@case.edu>
2016-09-07 13:37:38 -0400

    Update

--- HPCAnalysis/HPCDataAnalysis.py
+++ HPCAnalysis/HPCDataAnalysis.py
@@ -26,8 +26,8 @@ length = len(points);
 hppoints = hp.pixelfunc.vec2pix(nside, points[:,0],points[:,1],points[:,2]) #Assigning each point its HEALPix coordinate
 PixelTimes = np.zeros((N,len(hppoints))); #Each row contains a list of times at which rays struck the nth pixel
 PreDisplacementPrime = np.zeros((N,len(hppoints))); #Intermediate array to store ray initial condition dependent information
-PixelDisplacements = np.zeros((N,len(hppoints))); #Maximum Displacements measured in a pixel
-
+PixelDisplacements = np.zeros(len(hppoints)); #Maximum Displacements measured in a pixel
+PreDisplacement = np.zeros(N)
 m = numbers[0][0]
 n = numbers[0][1]
 L = numbers[0][2]
@@ -49,9 +49,11 @@ for i in range(0,N):
     ind = np.array(list(it.combinations(range(len(nz)),2)))
     d = np.abs(PixelTimes[i,ind[:,0]] - PixelTimes[i,ind[:,1]])
     for ii in np.where((d < T) & (d > 0))[0]:
-        PixelDisplacements[i,ind[ii,0]] += PreDisplacementPrime[i,ind[ii,1]]
-#Now we find the maximum displacement factor on each pixel
-PreDisplacement = ((np.amax(PixelDisplacements, axis=1))**.5) * (((9 * L) / (2 * np.pi * n * m * A))**.5) #Multiply by fmax**(1/2) * sigmaX * (vX**2) * p0**(-1) to get displacement
+        PixelDisplacements[ind[ii,0]] += PreDisplacementPrime[i][ind[ii,1]]
+    #Now we find the maximum displacement factor on each pixel
+    PreDisplacement[i] = (np.amax(PixelDisplacements)**.5) * (((9 * L) / (2 * np.pi * n * m * A))**.5) #Multiply by fmax**(1/2) * sigmaX * (vX**2) * p0**(-1) to get displacement
+    PixelDisplacements = np.zeros(len(hppoints))
+
 print(PreDisplacement)
 #We save the max displacements in each pixel (up to position independent info i.e. fmax**(1/2) * sigmaX * (vX**2) * p0**(-1))
 sio.savemat('PreDisplacement'+sys.argv[5]+'.mat', {'PreDisplacement':PreDisplacement})
