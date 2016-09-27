3273b675a158440baac7f2715e23d052a2918b73
David Cyncynates <dcc57@case.edu>
2016-09-26 21:58:32 -0400

    update

--- Simulation/Current/Pictures/HealpyIntensityMap.py
+++ Simulation/Current/Pictures/HealpyIntensityMap.py
@@ -7,7 +7,11 @@ import itertools as it
 import math
 
 #Loading Data from Matlab
-mat_contents = sio.loadmat('/Users/David/Macro-Impact/Simulation/Current/DataAnalysis/Mat'+ sys.argv[1] +'.mat');
-points = mat_contents['outputmat'];
+mat_contents = sio.loadmat('/Users/David/Macro-Impact/Simulation/Current/Results/Disp'+ sys.argv[1] +'.mat');
+Disp = mat_contents['Disp'];
 
 nside = int(sys.argv[2]);
+
+Intensities = hp.read_map(Disp)
+
+hp.mollview(Intensities, coord['G','E'])
